178f513c20704c2bc2307d8079d7e7b6e2a8ec0d
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-05 15:01:42 +0900

    20230605 NEW

---
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/constant/psgrkbn/PassengerKbnConst.kt
@@ -0,0 +1,33 @@
+package net.konohana.sakuya.maihime.enjurw.constant.psgrkbn
+
+/**
+ * ## 旅客種別定数クラス
+ * * 割引種別, 後払種別を定数で定義します。
+ * @author lafleurblanche
+ */
+class PassengerKbnConst  {
+    companion object {
+        /** 割引種別: 特定7割 */
+        const val PSGR_KBN_D001 = "特定7割"
+        /** 割引種別: 特七付添 */
+        const val PSGR_KBN_D002 = "特七付添"
+        /** 割引種別: 高校通学 */
+        const val PSGR_KBN_D003 = "高校通学"
+        /** 割引種別: 高校通信 */
+        const val PSGR_KBN_D004 = "通信制高校"
+        /** 割引種別: 身障者単独 */
+        const val PSGR_KBN_D005 = "身障者単独"
+        /** 割引種別: 身障者介助 */
+        const val PSGR_KBN_D006 = "身障介助者"
+        /** 割引種別: 被救護者 */
+        const val PSGR_KBN_D007 = "被救護者"
+        /** 後払種別: 私学特待 */
+        const val PSGR_KBN_PP001 = "私学特待"
+        /** 後払種別: 私学通学 */
+        const val PSGR_KBN_PP002 = "私学通学"
+        /** 後払種別: 幼年学校 */
+        const val PSGR_KBN_PP003 = "幼年学校"
+        /** 後払種別: 士官学校 */
+        const val PSGR_KBN_PP004 = "士官学校"
+    }
+}

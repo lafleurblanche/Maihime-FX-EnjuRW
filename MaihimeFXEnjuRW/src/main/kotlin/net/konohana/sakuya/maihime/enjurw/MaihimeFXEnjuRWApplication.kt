f3d1c64fcd61f98dadf83bbf2d204ace35e7e1bf
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-05 10:03:21 +0900

    20230605 First Commit

---
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/MaihimeFXEnjuRWApplication.kt
@@ -0,0 +1,20 @@
+package net.konohana.sakuya.maihime.enjurw
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class MaihimeFXEnjuRWApplication : Application() {
+    override fun start(stage: Stage) {
+        val fxmlLoader = FXMLLoader(MaihimeFXEnjuRWApplication::class.java.getResource("mainmenu/main-menu.fxml"))
+        val scene = Scene(fxmlLoader.load(), 1200.0, 900.0)
+        stage.title = "発券システム(仮)"
+        stage.scene = scene
+        stage.show()
+    }
+}
+
+fun main() {
+    Application.launch(MaihimeFXEnjuRWApplication::class.java)
+}

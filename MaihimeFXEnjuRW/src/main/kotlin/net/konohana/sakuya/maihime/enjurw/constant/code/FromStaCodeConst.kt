7da2652e991073a3333afe1805b840163f03464f
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-05 19:49:30 +0900

    20230605 NEW

---
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/constant/code/FromStaCodeConst.kt
@@ -0,0 +1,33 @@
+package net.konohana.sakuya.maihime.enjurw.constant.code
+
+class FromStaCodeConst  {
+    companion object {
+
+        const val FROMSTA_NAME_BTN01 = "梼谷"
+        const val FROMSTA_NAME_BTN02 = "東梼谷"
+        const val FROMSTA_NAME_BTN03 = "卯花"
+        const val FROMSTA_NAME_BTN04 = "上卯花"
+        const val FROMSTA_NAME_BTN05 = "槐谷"
+        const val FROMSTA_NAME_BTN06 = "苆谷"
+        const val FROMSTA_NAME_BTN07 = "拯川"
+        const val FROMSTA_NAME_BTN08 = "上拯川"
+        const val FROMSTA_NAME_BTN09 = "茄谷"
+        const val FROMSTA_NAME_BTN10 = "笥谷"
+        const val FROMSTA_NAME_BTN11 = "筏谷"
+        const val FROMSTA_NAME_BTN12 = "瑞堂"
+
+        const val FROMSTA_CODE_BTN01 = "RWXXXX"
+        const val FROMSTA_CODE_BTN02 = "RWXXXX"
+        const val FROMSTA_CODE_BTN03 = "RWXXXX"
+        const val FROMSTA_CODE_BTN04 = "RWXXXX"
+        const val FROMSTA_CODE_BTN05 = "RWXXXX"
+        const val FROMSTA_CODE_BTN06 = "RWXXXX"
+        const val FROMSTA_CODE_BTN07 = "RWXXXX"
+        const val FROMSTA_CODE_BTN08 = "RWXXXX"
+        const val FROMSTA_CODE_BTN09 = "RWXXXX"
+        const val FROMSTA_CODE_BTN10 = "RWXXXX"
+        const val FROMSTA_CODE_BTN11 = "RWXXXX"
+        const val FROMSTA_CODE_BTN12 = "RWXXXX"
+
+    }
+}

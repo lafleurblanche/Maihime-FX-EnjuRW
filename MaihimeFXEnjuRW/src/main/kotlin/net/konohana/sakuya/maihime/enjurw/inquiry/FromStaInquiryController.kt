071cd49ec9cd0ea7aa63a824e64553e40a80b08a
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-09 22:11:42 +0900

    20230609 イベント処理実装: Btn02

--- MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/inquiry/FromStaInquiryController.kt
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/inquiry/FromStaInquiryController.kt
@@ -63,6 +63,21 @@ class FromStaInquiryController {
 
     @FXML
     private fun onBtn02Click() {
+        // WebViewの宣言
+        val webView = WebView()
+        val webEngine = webView.engine
+        webEngine.isJavaScriptEnabled = true
+        webEngine.load(WebViewUrlConst.WEBVIEW_URL_ENJURW02)
+
+        val vbox = VBox()
+        vbox.children.addAll(webView)
+        val stage = Stage()
+        val scene = Scene(vbox)
+        stage.width = 400.0
+        stage.height = 650.0
+        stage.title = ""
+        stage.scene = scene
+        stage.show()
     }
 
     @FXML

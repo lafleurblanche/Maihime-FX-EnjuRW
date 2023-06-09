a86532a795e95571d3168bff260fd3fd666bc03f
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-09 22:11:14 +0900

    20230609 URL追加

--- MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/constant/webview/WebViewUrlConst.kt
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/constant/webview/WebViewUrlConst.kt
@@ -3,5 +3,8 @@ package net.konohana.sakuya.maihime.enjurw.constant.webview
 class WebViewUrlConst {
     companion object {
         const val WEBVIEW_URL_ENJURW01 = "http://localhost:8080/enjurw01"
+        const val WEBVIEW_URL_ENJURW02 = "http://localhost:8080/enjurw02"
+        const val WEBVIEW_URL_ENJURW03 = "http://localhost:8080/enjurw03"
+        const val WEBVIEW_URL_ENJURW04 = "http://localhost:8080/enjurw04"
     }
 }

52fef780b067a62810dbdf0efe943a385d18d994
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-23 23:12:14 +0900

    20230623 新規作成

---
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/constant/api/RequestDataConst.kt
@@ -0,0 +1,20 @@
+package net.konohana.sakuya.maihime.enjurw.constant.api
+
+/**
+ * APIリクエスト初期値定数クラス
+ * @author lafleurblanche
+ */
+class RequestDataConst {
+    companion object {
+        const val REQUEST_DEFAULT_FROM_STA = "乗車駅"
+        const val REQUEST_DEFAULT_TO_STA = "降車駅"
+        const val REQUEST_DEFAULT_VIA_ROUTE = "経路自動設定"
+        const val REQUEST_DEFAULT_MEMBER = "0"
+        const val REQUEST_DEFAULT_MONTH = "乗車月"
+        const val REQUEST_DEFAULT_DAY = "乗車日"
+        const val REQUEST_DEFAULT_TICKET_TYPE = "券種別"
+        const val REQUEST_DEFAULT_FROM_STA_CODE = "乗車駅コード"
+        const val REQUEST_DEFAULT_TO_STA_CODE = "降車駅コード"
+        const val REQUEST_DEFAULT_SECTOR_KBN = "ENJU"
+    }
+}

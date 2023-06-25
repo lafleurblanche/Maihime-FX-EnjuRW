46349002f4b3a6388250fbdb4d62f08bc9f4e7bf
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-06-25 17:50:18 +0900

    20230625 イベント処理追加

--- MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/utils/code/ToStaCodeSetter.kt
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/utils/code/ToStaCodeSetter.kt
@@ -64,3 +64,93 @@ fun getToStaCodeBtn06(): Pair<String, String> {
 fun getToStaCodeBtn07(): Pair<String, String> {
     return ToStaCodeConst.TOSTA_NAME_BTN07 to ToStaCodeConst.TOSTA_CODE_BTN07
 }
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN08(筏谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn08(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN08 to ToStaCodeConst.TOSTA_CODE_BTN08
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN09(中筏谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn09(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN09 to ToStaCodeConst.TOSTA_CODE_BTN09
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN10(蛞川)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn10(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN10 to ToStaCodeConst.TOSTA_CODE_BTN10
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN11(筮谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn11(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN11 to ToStaCodeConst.TOSTA_CODE_BTN11
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN12(中筮谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn12(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN12 to ToStaCodeConst.TOSTA_CODE_BTN12
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN13(上筮谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn13(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN13 to ToStaCodeConst.TOSTA_CODE_BTN13
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN14(筁谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn14(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN14 to ToStaCodeConst.TOSTA_CODE_BTN14
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN15(諾川)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn15(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN15 to ToStaCodeConst.TOSTA_CODE_BTN15
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN16(筥谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn16(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN16 to ToStaCodeConst.TOSTA_CODE_BTN16
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN17(本筥谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeBtn17(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_BTN17 to ToStaCodeConst.TOSTA_CODE_BTN17
+}

5f6adc18fe8b3d257092f2869cf0ab2b2a184158
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-07-10 00:25:08 +0900

    20230710 Add

--- MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/utils/code/ticket02/FromStaCodeSetter.kt
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/utils/code/ticket02/FromStaCodeSetter.kt
@@ -31,3 +31,53 @@ fun getFromStaCodeTicket02Btn02(): Pair<String, String> {
 fun getFromStaCodeTicket02Btn03(): Pair<String, String> {
     return FromStaCodeConst.FROMSTA_NAME_TICKET02_BTN03 to FromStaCodeConst.FROMSTA_CODE_TICKET02_BTN03
 }
+
+/**
+ * ## 乗車駅情報を取得します。
+ * * BTN04(宴谷)
+ * * Ticket02
+ * @return 乗車駅情報
+ */
+fun getFromStaCodeTicket02Btn04(): Pair<String, String> {
+    return FromStaCodeConst.FROMSTA_NAME_TICKET02_BTN04 to FromStaCodeConst.FROMSTA_CODE_TICKET02_BTN04
+}
+
+/**
+ * ## 乗車駅情報を取得します。
+ * * BTN05(上宴谷)
+ * * Ticket02
+ * @return 乗車駅情報
+ */
+fun getFromStaCodeTicket02Btn05(): Pair<String, String> {
+    return FromStaCodeConst.FROMSTA_NAME_TICKET02_BTN05 to FromStaCodeConst.FROMSTA_CODE_TICKET02_BTN05
+}
+
+/**
+ * ## 乗車駅情報を取得します。
+ * * BTN06(梼谷)
+ * * Ticket02
+ * @return 乗車駅情報
+ */
+fun getFromStaCodeTicket02Btn06(): Pair<String, String> {
+    return FromStaCodeConst.FROMSTA_NAME_TICKET02_BTN06 to FromStaCodeConst.FROMSTA_CODE_TICKET02_BTN06
+}
+
+/**
+ * ## 乗車駅情報を取得します。
+ * * BTN07(柊坂)
+ * * Ticket02
+ * @return 乗車駅情報
+ */
+fun getFromStaCodeTicket02Btn07(): Pair<String, String> {
+    return FromStaCodeConst.FROMSTA_NAME_TICKET02_BTN07 to FromStaCodeConst.FROMSTA_CODE_TICKET02_BTN07
+}
+
+/**
+ * ## 乗車駅情報を取得します。
+ * * BTN08(狒塚)
+ * * Ticket02
+ * @return 乗車駅情報
+ */
+fun getFromStaCodeTicket02Btn08(): Pair<String, String> {
+    return FromStaCodeConst.FROMSTA_NAME_TICKET02_BTN08 to FromStaCodeConst.FROMSTA_CODE_TICKET02_BTN08
+}

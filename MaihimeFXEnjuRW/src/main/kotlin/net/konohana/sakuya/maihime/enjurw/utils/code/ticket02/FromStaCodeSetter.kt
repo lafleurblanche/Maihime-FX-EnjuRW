4b167af0c92ec308b57e1b5d4c586017c8b54b0c
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-07-09 23:52:58 +0900

    20230709 新規作成

---
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/utils/code/ticket02/FromStaCodeSetter.kt
@@ -0,0 +1,33 @@
+package net.konohana.sakuya.maihime.enjurw.utils.code.ticket02
+
+import net.konohana.sakuya.maihime.enjurw.constant.code.FromStaCodeConst
+
+/**
+ * ## 乗車駅情報を取得します。
+ * * BTN01(茆川)
+ * * Ticket02
+ * @return 乗車駅情報
+ */
+fun getFromStaCodeTicket02Btn01(): Pair<String, String> {
+    return FromStaCodeConst.FROMSTA_NAME_TICKET02_BTN01 to FromStaCodeConst.FROMSTA_CODE_TICKET02_BTN01
+}
+
+/**
+ * ## 乗車駅情報を取得します。
+ * * BTN02(上茆川)
+ * * Ticket02
+ * @return 乗車駅情報
+ */
+fun getFromStaCodeTicket02Btn02(): Pair<String, String> {
+    return FromStaCodeConst.FROMSTA_NAME_TICKET02_BTN02 to FromStaCodeConst.FROMSTA_CODE_TICKET02_BTN02
+}
+
+/**
+ * ## 乗車駅情報を取得します。
+ * * BTN03(書石)
+ * * Ticket02
+ * @return 乗車駅情報
+ */
+fun getFromStaCodeTicket02Btn03(): Pair<String, String> {
+    return FromStaCodeConst.FROMSTA_NAME_TICKET02_BTN03 to FromStaCodeConst.FROMSTA_CODE_TICKET02_BTN03
+}

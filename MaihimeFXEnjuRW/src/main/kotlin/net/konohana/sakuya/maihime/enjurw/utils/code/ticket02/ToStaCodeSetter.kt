e3daede798fd33fbe50c33817a710d49826ea605
lafleurblanche <aquamint@squall.dti.ne.jp>
2023-07-10 09:12:19 +0900

    20230710 新規作成

---
+++ MaihimeFXEnjuRW/src/main/kotlin/net/konohana/sakuya/maihime/enjurw/utils/code/ticket02/ToStaCodeSetter.kt
@@ -0,0 +1,75 @@
+package net.konohana.sakuya.maihime.enjurw.utils.code.ticket02
+
+import net.konohana.sakuya.maihime.enjurw.constant.code.ToStaCodeConst
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN01(茆川)
+ * @return 降車駅情報
+ */
+fun getToStaCodeTicket02Btn01(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_TICKET02_BTN01 to ToStaCodeConst.TOSTA_CODE_TICKET02_BTN01
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN02(上茆川)
+ * @return 降車駅情報
+ */
+fun getToStaCodeTicket02Btn02(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_TICKET02_BTN02 to ToStaCodeConst.TOSTA_CODE_TICKET02_BTN02
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN03(書石)
+ * @return 降車駅情報
+ */
+fun getToStaCodeTicket02Btn03(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_TICKET02_BTN03 to ToStaCodeConst.TOSTA_CODE_TICKET02_BTN03
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN04(宴谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeTicket02Btn04(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_TICKET02_BTN04 to ToStaCodeConst.TOSTA_CODE_TICKET02_BTN04
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN05(上宴谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeTicket02Btn05(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_TICKET02_BTN05 to ToStaCodeConst.TOSTA_CODE_TICKET02_BTN05
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN06(梼谷)
+ * @return 降車駅情報
+ */
+fun getToStaCodeTicket02Btn06(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_TICKET02_BTN06 to ToStaCodeConst.TOSTA_CODE_TICKET02_BTN06
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN07(柊坂)
+ * @return 降車駅情報
+ */
+fun getToStaCodeTicket02Btn07(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_TICKET02_BTN07 to ToStaCodeConst.TOSTA_CODE_TICKET02_BTN07
+}
+
+/**
+ * ## 降車駅情報を取得します。
+ * * BTN08(狒塚)
+ * @return 降車駅情報
+ */
+fun getToStaCodeTicket02Btn08(): Pair<String, String> {
+    return ToStaCodeConst.TOSTA_NAME_TICKET02_BTN08 to ToStaCodeConst.TOSTA_CODE_TICKET02_BTN08
+}
